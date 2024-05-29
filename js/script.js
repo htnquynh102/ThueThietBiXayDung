ca288b04e9144c632bc619cdb9e48a917acaf005
Kim Thang <thanghuynh.271203@gmail.com>
2024-05-29 15:57:44 +0700

    Create ui quan ly thiet bi va quan ly phieu dat cho admin

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown-filter");
+    if (dropdown.style.display === "none" || dropdown.style.display === "") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
+}
+function toggleDropdownMenu() {
+    var dropdownMenu = document.getElementById("dropdown-menu");
+    if (dropdownMenu.style.display === "none" || dropdownMenu.style.display === "") {
+        dropdownMenu.style.display = "flex";
+    } else {
+        dropdownMenu.style.display = "none";
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    const rows = document.querySelectorAll('tbody tr');
+    rows.forEach(row => {
+        const statusCell = row.querySelector('td span');
+        if (statusCell.textContent.trim() === 'Đã xác nhận') {
+            statusCell.style.backgroundColor = 'green';
+            statusCell.style.color = 'white';
+        } else if (statusCell.textContent.trim() === 'Đang chờ xác nhận') {
+            statusCell.style.backgroundColor = '#a74444d7';
+            statusCell.style.color = 'white';
+        }
+    });
+});
